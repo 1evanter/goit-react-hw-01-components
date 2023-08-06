93617c089e6256afde6a97fd4aff58b3641f55b2
1evanter <rita.oleksenkoo@gmail.com>
2023-08-06 22:10:48 +0200

    add global styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from "./Profile/Profile";
 import profileData from "../user.json"
+import { GlobalStyle } from "./GlobalStyle";
 
 
 export const App = () => {
   return <div>
     <Profile data={profileData} />
+    <GlobalStyle/>
   </div>
 };
