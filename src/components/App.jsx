976436e439fdc344e73770e4384140bebf3a12d1
1evanter <rita.oleksenkoo@gmail.com>
2023-08-06 22:45:03 +0200

    create Statistics component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import { Profile } from "./Profile/Profile";
-import profileData from "../user.json"
 import { GlobalStyle } from "./GlobalStyle";
+import { Statistics } from "./Statistics/Statistics";
+
+import profileData from "../user.json";
+import statisticsData from "../data.json";
 
 
 export const App = () => {
   return <div>
     <Profile data={profileData} />
+    <Statistics data={statisticsData}/>
     <GlobalStyle/>
   </div>
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,5 @@
+export const Statistics = ({data}) => {
+    return (
+       data.map(data => <div key={data.id}>34987395</div>) 
+    )
+}
