ddfff58b9934a2cf666cfabf7509004763c87a12
1evanter <rita.oleksenkoo@gmail.com>
2023-08-15 09:40:24 +0300

    fixed Statistics component

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,15 +1,14 @@
-import { List, Section, Title } from './Statistic.styled'
-import { StatisticsItem } from './StatisticsItem'
-
-
-
+import { List, Section, Title, Item, Span } from './Statistic.styled'
 
 export const Statistics = ({ stats }) => {
     return (
         <Section>
               <Title>Upload stats</Title>
               <List>
-                <StatisticsItem stats={stats} />
+                {   stats.map(({id, label, percentage}) => <Item key={id}>
+            <span>{label}</span>
+            <Span>{percentage}%</Span>
+        </Item>)  }
         </List>
         </Section>
       
